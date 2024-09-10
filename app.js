25d0f0ac7b00654338a27028a509bba7af1ef34f
Chasky91 <david_c91@outlook.com>
2024-09-10 20:06:39 -0300

    app cafeteria con ruta raiz

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-import express from 'express'
-
+import express from "express"
 
 const app = express()
+const PUERTO = 5000
 
-const PORT = 5000
-
+//http://localhost:5000"/"
+//La linea siguiente es un endpoint
 app.get('/', (req, res) => {
 
-    res.send('<h1> Hola mundo </h1>')
+    res.send("<h1>Hola CENT 44</h1>");
+    //res.json({})
 })
 
-
-app.listen(PORT, 
-    console.log( `App  corriendo por el puerto http://localhost:${PORT}`)
-)
+app.listen(PUERTO, () => {
+    console.log('corriendo por el puerto ', PUERTO)
+})
