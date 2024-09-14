bec1a578eb2009a8f82b4be9d80dd51e5c70173a
Chasky91 <david_c91@outlook.com>
2024-09-13 21:43:52 -0300

    clase 13/09

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 import express from "express"
+import clientes from "./clientes.js"
 
 const app = express()
 const PUERTO = 5000
 
-//http://localhost:5000"/"
+
+//http://localhost:5000/"
 //La linea siguiente es un endpoint
 app.get('/', (req, res) => {
 
+    //primera respuesta
     res.send("<h1>Hola CENT 44</h1>");
-    //res.json({})
+    //segunda respuesta Prohibida arroja errores
+    //res.send("<h1>Hola CENT 4</h1>");
 })
+
+//http://localhost:5000/cliente
+//La linea siguiente es un endpoint
+app.get('/cliente', (req, res) => {
+    
+    res.send("End point para todos los clientes")
+})
+
 
 app.listen(PUERTO, () => {
     console.log('corriendo por el puerto ', PUERTO)
---
+++ clientes.js
@@ -0,0 +1,14 @@
+const clientes = [
+    {
+        "nombre": "Jose",
+        "Apellido":"Ruiz",
+        "email":"joseruiz@gmail.com"
+    },
+    {
+        "nombre": "Esteba",
+        "Apellido":"Quito",
+        "email":"estebanQuito@gmail.com"
+    }    
+]
+
+export default clientes
